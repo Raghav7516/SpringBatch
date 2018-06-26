e73f235caa5f7ad41d4195c40d15988c1b8d9e25
pawan_kushwah <pawan.k@contus.in>
2018-06-26 17:46:15 +0530

    http://localhost:8080/launchjob link to test

---
+++ src/main/java/com/web/controller/JobLauncherController.java
@@ -0,0 +1,32 @@
+package com.web.controller;
+ 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class JobLauncherController {
+ 
+    @Autowired
+    JobLauncher jobLauncher;
+ 
+    @Autowired
+    Job job;
+ 
+    @RequestMapping("/launchjob")
+    public String handle() throws Exception {
+ 
+        try {
+            JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            jobLauncher.run(job, jobParameters);
+        } catch (Exception e) {
+        }
+ 
+        return "Done";
+    }
+}
